0d70a656f5397900f4d0a77f5f889b421302b57c
Graps <twinklierre@gmail.com>
2024-10-21 21:06:10 +0700

    DES424-18 added frontend project

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    let text: String = ""
+    function sendToBackend(): void {
+        console.log(text);
+    }
+</script>
+
+<h1>Skeleton Project</h1>
+<input type="text" bind:value={text}>
+<input type="button" value="enter" on:click={sendToBackend}>
