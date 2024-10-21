b3e4ea515f080b16e5181b9cfe07afe11ad22758
Graps <twinklierre@gmail.com>
2024-10-21 21:20:57 +0700

    DES424-18 added frontend fetch request

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -1,1 +1,15 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+export async function post_api_ping(text: string): Promise<string> {
+    const res = await fetch("/api/ping", {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        method: 'POST',
+        body: JSON.stringify({text: text})
+    })
+    
+    const json = await res.json();
+    return JSON.stringify(json);
+}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
-    let text: String = ""
-    function sendToBackend(): void {
-        console.log(text);
+    import { post_api_ping } from "$lib";
+    let text: string = ""
+    let ping_text: string = ""
+    
+    async function sendToBackend(): Promise<void> {
+        ping_text = await post_api_ping(text);
     }
 </script>
 
 <h1>Skeleton Project</h1>
 <input type="text" bind:value={text}>
 <input type="button" value="enter" on:click={sendToBackend}>
+ping from server = {ping_text}
