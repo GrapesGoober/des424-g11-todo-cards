ec7787fd334e7f079d8da32e6e8dabab51b0e6ad
Graps <twinklierre@gmail.com>
2024-11-23 18:22:59 +0700

    modified vite.config files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,19 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import {defineConfig} from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    server: {
-        watch: {
-            usePolling: true,
-        },
-        host: true, // needed for the DC port mapping to work
-        strictPort: true,
-        port: 5173,
-        proxy: {
-			'/api/': {
-				target: 'http://fastapi-app:8000'
-			}
-		}
-    }
+	plugins: [sveltekit()]
 });
