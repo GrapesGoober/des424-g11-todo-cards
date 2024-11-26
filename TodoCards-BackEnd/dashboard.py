448dd94e04cffd03f29af10ebae0142434f0f00d
siitaccount2712 <6422782712@g.siit.tu.ac.th>
2024-11-26 23:45:28 +0700

    select finished all form db

---
+++ TodoCards-BackEnd/dashboard.py
@@ -0,0 +1,45 @@
+import admin
+import random
+import string
+from datetime import datetime
+
+def get_finished_cards(mydb, deck_id):
+    # Create a cursor to execute the query
+    mycursor = mydb.cursor()
+
+    # SQL query to select finished cards
+    mycursor.execute("""
+        SELECT count(cardid)
+        FROM card
+        WHERE deckId = %s AND cardIsFinished = 1
+    """, (deck_id,))
+
+    # Fetch all the results
+    finished_cards = mycursor.fetchall()
+
+    # Close the cursor
+    mycursor.close()
+
+    # Return the finished cards
+    return finished_cards
+
+def get_all_cards(mydb, deck_id):
+    # Create a cursor to execute the query
+    mycursor = mydb.cursor()
+
+    # SQL query to select all cards
+    mycursor.execute("""
+        SELECT count(cardid)
+        FROM card
+        WHERE deckId = %s
+    """, (deck_id,))
+
+    # Fetch all the results
+    all_cards = mycursor.fetchall()
+
+    # Close the cursor
+    mycursor.close()
+
+    # Return all cards
+    return all_cards
+
