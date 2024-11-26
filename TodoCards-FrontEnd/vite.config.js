4f9db8b7392c799eeba886afd4ff73943fb3f479
Graps <twinklierre@gmail.com>
2024-11-26 22:18:30 +0700

    add hot reload to vite config

--- TodoCards-FrontEnd/vite.config.js
+++ TodoCards-FrontEnd/vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()],
+	watch: {
+		usePolling: true,
+	},
 	server: {
 		proxy: {
 			'/api': 'http://flask-app:5000'
